76ade5d2ec4b1f0301dc7e3182416feb5f92d780
Julio Muller <juliodolago@gmail.com>
2020-12-15 12:29:01 -0300

    Renamed module

--- rules/index.js
+++ rules/index.js
@@ -1,13 +1,13 @@
+const possibleErrors = require('./possible-errors')
 const bestPractices = require('./best-practices')
 const variables = require('./variables')
-const errors = require('./errors')
 const style = require('./style')
 const es6 = require('./es6')
 
 module.exports = {
+  ...possibleErrors,
   ...bestPractices,
   ...variables,
-  ...errors,
   ...style,
   ...es6,
 }

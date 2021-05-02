7397eb5c6aff7082150688e9c5560aa7b7d51fed
Julio Muller <juliodolago@gmail.com>
2021-05-01 22:46:44 -0300

    Created a configuration for React Native

---
+++ packages/react-native/rules/plugins/react-native/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-color-literals.md
+  'no-color-literals': 'off',
+
+  // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-inline-styles.md
+  'no-inline-styles': 'off',
+
+  // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-raw-text.md
+  'no-raw-text': ['error', {
+    skip: [],
+  }],
+
+  // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-single-element-style-arrays.md
+  'no-single-element-style-arrays': 'error',
+
+  // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-unused-styles.md
+  'no-unused-styles': 'warn',
+
+  // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/sort-styles.md
+  'sort-styles': 'off',
+
+  // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/split-platform-components.md
+  'split-platform-components': ['warn', {
+    androidPathRegex: '\\.android.(js|jsx|ts|tsx)$',
+    iosPathRegex: '\\.ios.(js|jsx|ts|tsx)$',
+  }],
+}
